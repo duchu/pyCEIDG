ea0e4274281e52adfebe06a5978d76b98fb04d73
karabanb <bkarabands@gmail.com>
2019-11-24 12:19:32 +0100

    last commit before share code for Asia

--- CeidgRequest.py
+++ CeidgRequest.py
@@ -3,7 +3,6 @@ from zeep import Client
 import xmltodict
 import pickle
 import config
-import random
 import json
 from datetime import date, timedelta
 from data import ceidg_api as api
@@ -13,14 +12,16 @@ password = config.password
 url = 'https://datastore.ceidg.gov.pl/CEIDG.DataStore/services/NewDataStoreProvider.svc?wsdl'
 cl = Client(wsdl=url)
 
-start_date = date(2012, 1, 1)
+# Defining requested time range of data --------------------------------------------------------------------------------
+
+start_date = date(2012, 6, 1)
 end_date = date(2018, 11, 1)
 delta = timedelta(weeks=10)
 
 results = []
 
 while start_date < end_date:
-    req_large_data = {'AuthToken': password, 'DateFrom': f'{start_date}', 'DateTo': f'{start_date + delta}'}
+    req_large_data = {'AuthToken': config.password, 'DateFrom': f'{start_date}', 'DateTo': f'{start_date + delta}'}
     response = cl.service.GetID(**req_large_data)
     res_dict = xmltodict.parse(response)
     res_dict = res_dict['WynikWyszukiwania']['IdentyfikatorWpisu']
@@ -28,32 +29,33 @@ while start_date < end_date:
     start_date += delta
     print('DateFrom: ' f'{start_date}', ', DateTo: ' f'{start_date + delta} passed!')
 
-with open('ceidg_ids.pickle', 'wb') as file:
-    file.write(pickle.dumps(results))
+# with open('ceidg_ids.pickle', 'wb') as file:
+#     file.write(pickle.dumps(results))
 
 ceidg_ids = open('ceidg_ids.pickle', 'rb')
 ceidg_ids = pickle.load(ceidg_ids)
 
-random.seed(123)
-sampled_ids = random.choices(ceidg_ids, k=500)
+j = 1000
+k = 3000
 
-j = 30000
-k = 33000
-
-while k <= 33000:
+while k <= 20000:
     api.ask_with_args(config.password, 'UniqueId', ceidg_ids[j:k], path=os.getcwd() + '/downloaded_nips/')
-    j += 1000
-    k += 1000
+    j += 2000
+    k += 2000
 
+# Prepraring data for mongoimport --------------------------------------------------------------------------------------
 
-with open('downloaded_nips/ceidg_111119_230127.json') as file:
-    data = file.read()
+files = os.listdir('downloaded_nips')
+files.remove('.DS_Store')
 
-obj = json.loads(data)
+for file in files:
+    with open(os.path.join(os.path.abspath('downloaded_nips'), file)) as f:
+        data = f.read()
+        obj = json.loads(data)
 
-for i in range(len(obj)):
-    obj[i]['_id'] = obj[i]['IdentyfikatorWpisu']
-    obj[i].pop('IdentyfikatorWpisu')
+        for i in range(len(obj)):
+            obj[i]['_id'] = obj[i]['IdentyfikatorWpisu']
+            obj[i].pop('IdentyfikatorWpisu')
 
-with open('downloaded_nips/ceidg_111119_230127.json', 'w') as file:
-    json.dump(obj, file, indent=2)
+    with open(os.path.join(os.path.abspath('downloaded_nips'), file), 'w') as f:
+        json.dump(obj, f, indent=2)
---
+++ scripts/100_LoadingData.py
@@ -0,0 +1,6 @@
+
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+db = myclient.get_database('ceidg')
